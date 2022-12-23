22ccc96dd35a962a039b4fa372313b0f34f6d46e
radkoff26 <radko.slava@mail.ru>
2022-12-23 15:20:50 +0300

    add more tests

--- src/test/java/com/radkoff26/calculus/resolver/DerivativeResolverTest.java
+++ src/test/java/com/radkoff26/calculus/resolver/DerivativeResolverTest.java
@@ -7,6 +7,7 @@ import com.radkoff26.calculus.calculator.ExpressionCalculator;
 import com.radkoff26.calculus.model.Expression;
 
 class DerivativeResolverTest {
+    public static final double DELTA = 1e-6;
     private final Resolver<String> expressionResolver = new ExpressionResolver();
     private final Resolver<Expression> derivativeResolver = new DerivativeResolver();
     private final ExpressionCalculator expressionCalculator = new ExpressionCalculator("x");
@@ -17,6 +18,19 @@ class DerivativeResolverTest {
 
     @Test
     void basicDerivativeTest() {
-        Assertions.assertEquals(5, calculate("x ^ 3 + x ^ 2 - 1", 1), 1e-6);
+        Assertions.assertEquals(5, calculate("x ^ 3 + x ^ 2 - 1", 1), DELTA);
+    }
+
+    @Test
+    void trigonometricSinAndCosTest() {
+        String s = "cos(x) * (2 * x + 4)";
+        Assertions.assertEquals(2, calculate(s, 0), DELTA);
+        Assertions.assertEquals(-Math.PI - 4, calculate(s, Math.PI / 2), DELTA);
+    }
+
+    @Test
+    void exponentialTest() {
+        Assertions.assertEquals(192, calculate("(x ^ 2) ^ 3", 2), DELTA);
+        Assertions.assertEquals(1, calculate("x ^ x", 1), DELTA);
     }
 }
